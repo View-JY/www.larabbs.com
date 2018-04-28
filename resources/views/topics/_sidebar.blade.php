9daf5c1d7bf0838140340a35ae5940e560053078
View-JY <1154930416@qq.com>
2018-04-28 15:18:41 +0800

    Category topic list

--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,1 +1,10 @@
-右边导航栏
+<div class="list-group">
+    <a href="{{ route('topics.index') }}" class="list-group-item {{ active_class(if_route('topics.index')) }}">
+      话题
+    </a>
+    <a href="{{ route('categories.show', 1) }}" class="list-group-item {{ active_class((if_route('categories.show') && if_route_param('category', 1))) }}">分享</a>
+    <a href="{{ route('categories.show', 2) }}" class="list-group-item {{ active_class((if_route('categories.show') && if_route_param('category', 2))) }}">教程</a>
+    <a href="{{ route('categories.show', 3) }}" class="list-group-item {{ active_class((if_route('categories.show') && if_route_param('category',
+    	3))) }}">问答</a>
+    <a href="{{ route('categories.show', 4) }}" class="list-group-item {{ active_class((if_route('categories.show') && if_route_param('category', 4))) }}">公告</a>
+</div>
